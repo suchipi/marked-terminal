d7dde1e8d570a3e3036b8b1b4e4e0ec714f16e5a
Lily Skye <me@suchipi.com>
2023-09-04 01:37:51 -0600

    should verify that works via a test, though

---
+++ tests/e2e-custom-syntax-highlight.test.js
@@ -0,0 +1,33 @@
+process.env.FORCE_COLOR = 3;
+
+import { readFileSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { describe, it, expect } from 'vitest';
+import Renderer from '../index.js';
+import marked from './_marked.js';
+import { fileURLToPath } from 'url';
+
+function getFixtureFile(fileName) {
+  return readFileSync(
+    resolve(dirname(fileURLToPath(import.meta.url)), 'fixtures/', fileName),
+    { encoding: 'utf8' }
+  );
+}
+
+function markup(str) {
+  var r = new Renderer({
+    syntaxHighlighters: {
+      javascript: (code) => {
+        return '<JS_START>' + code + '<JS_END>';
+      }
+    }
+  });
+  return marked(str, { renderer: r });
+}
+
+describe('e2e', function () {
+  it('should render a document full of different supported syntax', function () {
+    const actual = markup(getFixtureFile('e2e.md'));
+    expect(actual).toMatchSnapshot();
+  });
+});
